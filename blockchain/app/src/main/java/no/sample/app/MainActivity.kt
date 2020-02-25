e00aed35a97ba1bf7c3da48e6268cc3f23a11ad1
Imran Baig <imranbaig@hotmail.co.uk>
2020-02-25 12:58:45 +0100

    async programming solution for long running code

--- blockchain/app/src/main/java/no/sample/app/MainActivity.kt
+++ blockchain/app/src/main/java/no/sample/app/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     var blockchain = Blockchain() // Blockchain Data structure
 
     var blockNo = 1;            // Initial block number which will be incremented with one after the block is mined and added to chain
-    val difficultyLevel = 4     // This number dictate what would be the  time complexity of the block mining algorithm
+    val difficultyLevel = 4     // This number dictates what would be the  time complexity of the block mining algorithm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +22,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         floatingActionButton.setOnClickListener{
+            Thread( MyRunnable() ).start() // Create & Start a new thread with MyRunnable class instance
+        }
 
-            progressBar.visibility = View.VISIBLE // Show progress bar spinning wheel
+    }
+
+    inner class MyRunnable: Runnable { // An inner class that implements Runnable interface
+
+        public override fun run() {
+
+            this@MainActivity.runOnUiThread { // Run on ui thread using activity runOnUiThread
+                progressBar.visibility = View.VISIBLE // Show progress bar spinning wheel
+            }
 
             var block = Block("Block No: $blockNo" , difficultyLevel); // Creating a block that will be mined and added to the block chain data structure
 
+            //start of Unprotected shared data (Critical section)
             blockchain.mineAndAdd( block ) // mining and adding block
             blockNo ++
+            //end of Unprotected shared data
 
-            textViewConsole.text = blockchain.toString() // turning the blockchain information into a string
+            this@MainActivity.runOnUiThread { // Run on ui thread using activity runOnUiThread
+                textViewConsole.text = blockchain.toString() // turning the blockchain information into a string
+                progressBar.visibility = View.INVISIBLE // Stop progress bar spinning wheel
 
-            progressBar.visibility = View.INVISIBLE // Stop progress bar spinning wheel
-
+            }
         }
-
     }
 }
