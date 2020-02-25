7c203a83e5db5aa4e52c86b8597c2b53be76ee34
Imran Baig <imranbaig@hotmail.co.uk>
2020-02-25 14:47:45 +0100

    used async task

--- blockchain/app/src/main/java/no/sample/app/MainActivity.kt
+++ blockchain/app/src/main/java/no/sample/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package no.sample.app
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import no.sample.app.blockchain.Block
@@ -21,32 +23,42 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        Log.d("threadName", Thread.currentThread().name)
+
         floatingActionButton.setOnClickListener{
-            Thread( MyRunnable() ).start() // Create & Start a new thread with MyRunnable class instance
+
+            progressBar.visibility = View.VISIBLE // Show progress bar spinning wheel
+
+            var task = MyAsyncTask();
+            task.execute();
+
         }
 
     }
 
-    inner class MyRunnable: Runnable { // An inner class that implements Runnable interface
+    inner class MyAsyncTask:AsyncTask<Any, Any, Block> () {
 
-        public override fun run() {
+        override fun doInBackground(vararg params: Any?): Block {
 
-            this@MainActivity.runOnUiThread { // Run on ui thread using activity runOnUiThread
-                progressBar.visibility = View.VISIBLE // Show progress bar spinning wheel
-            }
+            Log.d("threadName", Thread.currentThread().name)
 
-
-            //start of Unprotected shared data (Critical section)
             var block = Block("Block No: $blockNo" , difficultyLevel); // Creating a block that will be mined and added to the block chain data structure
             blockchain.mineAndAdd( block ) // mining and adding block
             blockNo ++
-            //end of Unprotected shared data
 
-            this@MainActivity.runOnUiThread { // Run on ui thread using activity runOnUiThread
-                textViewConsole.text = blockchain.toString() // turning the blockchain information into a string
-                progressBar.visibility = View.INVISIBLE // Stop progress bar spinning wheel
+            return block
+        }
 
-            }
+
+        override fun onPostExecute(block: Block?) {
+            super.onPostExecute(block)
+
+            Log.d("blockchain", "####onPostExecute##### " + block?.data)
+
+            textViewConsole.text = blockchain.toString() // turning the blockchain information into a string
+            progressBar.visibility = View.INVISIBLE // Stop progress bar spinning wheel
         }
     }
+
+
 }
